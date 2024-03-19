ebbf17e47779525da10cd1797bb9abdef7d25df8
tto <myatthta2019@gmail.com>
2024-03-19 21:28:22 +0630

    calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+from msvcrt import getch
+
+print("This is Sample Pthon Calculator.")
+print("Please Enter First Numer.")
+x = int(input())
+print("Please Enter Second Numer.")
+y = int(input())
+z = x+y
+print(f"Your Numer is {z}.")
+print("Press any key to continue...")
+_ = getch()
+exit()
